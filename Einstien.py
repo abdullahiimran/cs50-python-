b76d8e1d4a93c342dbe425328adcdbef321678c0
abdullahiimran <abdullahiimrann79@gmail.com>
2026-07-12 13:32:33 +0500

    Add files via upload
    This Python program prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer, assuming that the user will input an integer.

---
+++ Einstien.py
@@ -0,0 +1,3 @@
+print("Einstien's famous equation says:𝐸=𝑚⁢𝑐^2")
+m = int(input("Enter the mass of the object: "))
+print(f"The value of Energy (in joules) is: {m * 300000000}, Where c is the speed of light and is equal to 300,000,000 ")
